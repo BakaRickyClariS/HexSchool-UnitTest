afaf249c860b2ccd3df47a9000cacd369ef69ee0
BakaRickyClariS <ricky860526@gmail.com>
2025-09-18 15:25:49 +0800

    first commit and install vitest

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    include: ["src/**/*.test.{ts,tsx}"], // 確保包含 __tests__ 資料夾
+  },
+});
